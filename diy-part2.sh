bde2edf75437e9772022a1a824034dd7a18a9f68
wwz09 <wwz5124@gmail.com>
2021-02-12 19:43:59 +0800

    更新0212

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,5 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/OpenWrt/Kyara/g' package/base-files/files/bin/config_generate
 # Modify default wireless name
 sed -i 's/OpenWrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
