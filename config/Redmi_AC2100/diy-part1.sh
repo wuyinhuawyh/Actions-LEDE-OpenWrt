7e59c5ccf2c5853c872e750817a10205daa97b55
wwz09 <wwz5124@gmail.com>
2021-02-13 14:07:17 +0800

    更新0213

--- config/Redmi_AC2100/diy-part1.sh
+++ config/Redmi_AC2100/diy-part1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# File name: diy-part3.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
@@ -17,4 +17,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 # Add a feed source
-sed -i '$a src-git wwz09 https://github.com/wwz09/wwz09-packages.git' feeds.conf.default
+#sed -i '$a src-git wwz09 https://github.com/wwz09/wwz09-packages.git' feeds.conf.default
